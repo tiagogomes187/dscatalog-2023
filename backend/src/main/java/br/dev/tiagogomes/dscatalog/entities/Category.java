5d31e8a7f7032440fc7efa03df330404a5b8a6da
Tiago Gomes <tiagogomes187@gmail.com>
2023-06-17 23:15:16 -0300

    CategoryResource

---
+++ backend/src/main/java/br/dev/tiagogomes/dscatalog/entities/Category.java
@@ -0,0 +1,53 @@
+package br.dev.tiagogomes.dscatalog.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID;
+
+    static {
+        serialVersionUID = 1L;
+    }
+
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
